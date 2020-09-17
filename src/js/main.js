f3c29b1a6ef7d59226396d09b5c1ec443d849dab
Ayana Osawa <ayana.osawa@gmail.com>
2020-09-17 16:59:40 -0400

    Hide three code

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,39 @@
-var width = window.innerWidth
-var height = window.innerHeight
+// var width = window.innerWidth
+// var height = window.innerHeight
 
-var renderer = new THREE.WebGLRenderer({ antialias: true })
-renderer.setSize(width, height)
-const element = document.querySelector(".element")
-element.appendChild(renderer.domElement)
+// var renderer = new THREE.WebGLRenderer({ antialias: true })
+// renderer.setSize(width, height)
+// const element = document.querySelector(".element")
+// element.appendChild(renderer.domElement)
 
-var scene = new THREE.Scene()
+// var scene = new THREE.Scene()
 
-var cubeGeometry = new THREE.CubeGeometry(100, 100, 100)
-var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff })
-var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-cube.rotation.y = (Math.PI * 45) / 180
-scene.add(cube)
+// var cubeGeometry = new THREE.CubeGeometry(100, 100, 100)
+// var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff })
+// var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+// cube.rotation.y = (Math.PI * 45) / 180
+// scene.add(cube)
 
-var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000)
-camera.position.y = 160
-camera.position.z = 400
-camera.lookAt(cube.position)
+// var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000)
+// camera.position.y = 160
+// camera.position.z = 400
+// camera.lookAt(cube.position)
 
-scene.add(camera)
+// scene.add(camera)
 
-var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000)
-var skyboxMaterial = new THREE.MeshBasicMaterial({
-  color: 0x000000,
-  side: THREE.BackSide,
-})
-var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial)
+// var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000)
+// var skyboxMaterial = new THREE.MeshBasicMaterial({
+//   color: 0x000000,
+//   side: THREE.BackSide,
+// })
+// var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial)
 
-scene.add(skybox)
+// scene.add(skybox)
 
-var pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(0, 300, 200)
+// var pointLight = new THREE.PointLight(0xffffff)
+// pointLight.position.set(0, 300, 200)
 
-scene.add(pointLight)
+// scene.add(pointLight)
 
 // var clock = new THREE.Clock()
 
@@ -77,6 +77,6 @@ function onClick() {
       .catch(console.error)
   } else {
     // handle regular non iOS 13+ devices
-    document.body.style.background = "blue"
+    document.body.style.background = "yellow"
   }
 }
