15a752c7552e42ea3cbdfee4a4ff402376308e50
Ayana Osawa <ayana.osawa@gmail.com>
2020-09-17 17:06:21 -0400

    Update to green

--- docs/js/app.js
+++ docs/js/app.js
@@ -43,15 +43,12 @@ function onClick() {
     DeviceOrientationEvent.requestPermission().then(function (permissionState) {
       if (permissionState === "granted") {
         window.addEventListener("deviceorientation", function (e) {
-          // requestAnimationFrame(render)
-          // cube.rotation.y += e.beta
-          // renderer.render(scene, camera)
           element.style.transform = "rotateZ(" + (e.alpha - 180) + "deg) " + "rotateX(" + e.beta + "deg) " + "rotateY(" + -e.gamma + "deg)";
         });
       }
     })["catch"](console.error);
   } else {
     // handle regular non iOS 13+ devices
-    document.body.style.background = "yellow";
+    document.body.style.background = "green";
   }
 }
