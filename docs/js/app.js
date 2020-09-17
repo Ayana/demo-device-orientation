641749327c500b0877964e838234cdf774cae324
Ayana Osawa <ayana.osawa@gmail.com>
2020-09-17 16:03:45 -0400

    Try three.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,8 +1,30 @@
 "use strict";
 
-var button = document.querySelector(".button");
-console.log(button);
-button.addEventListener("click", onClick);
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
+var renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+var element = document.querySelector(".element");
+element.appendChild(renderer.domElement);
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({
+  color: 0xff0000
+});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+cube.position.z = -5;
+cube.rotation.x = 10;
+cube.rotation.y = 5;
+renderer.render(scene, camera); // const animate = function () {
+//   cube.rotation.x += 0.01
+//   renderer.render(scene, camera)
+//   requestAnimationFrame(animate)
+// }
+// animate()
+
+element.addEventListener("click", onClick);
 
 function onClick() {
   // feature detect
@@ -10,8 +32,16 @@ function onClick() {
     DeviceOrientationEvent.requestPermission().then(function (permissionState) {
       if (permissionState === "granted") {
         window.addEventListener("deviceorientation", function (e) {
-          var el = document.querySelector(".element");
-          el.style.transform = "rotateZ(" + (e.alpha - 180) + "deg) " + "rotateX(" + e.beta + "deg) " + "rotateY(" + -e.gamma + "deg)";
+          var animate = function animate() {
+            cube.rotation.x += e.beta;
+            renderer.render(scene, camera);
+            requestAnimationFrame(animate);
+          };
+
+          animate(); // element.style.transform =
+          //   "rotateZ(" + (e.alpha - 180) + "deg) " + 
+          //   "rotateX(" + e.beta + "deg) " +
+          //   "rotateY(" + -e.gamma + "deg)"
         });
       }
     })["catch"](console.error);
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,33 @@
-const button = document.querySelector(".button")
-console.log(button)
-button.addEventListener("click", onClick)
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+)
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+const element = document.querySelector(".element")
+element.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+cube.position.z = -5
+cube.rotation.x = 10
+cube.rotation.y = 5
+renderer.render(scene, camera)
+
+// const animate = function () {
+//   cube.rotation.x += 0.01
+//   renderer.render(scene, camera)
+//   requestAnimationFrame(animate)
+// }
+// animate()
+
+
+element.addEventListener("click", onClick)
 
 function onClick() {
   // feature detect
@@ -9,17 +36,17 @@ function onClick() {
       .then((permissionState) => {
         if (permissionState === "granted") {
           window.addEventListener("deviceorientation", (e) => {
-            const el = document.querySelector(".element")
-            el.style.transform =
-              "rotateZ(" +
-              (e.alpha - 180) +
-              "deg) " +
-              "rotateX(" +
-              e.beta +
-              "deg) " +
-              "rotateY(" +
-              -e.gamma +
-              "deg)"
+            const animate = function () {
+              cube.rotation.x += e.beta
+              renderer.render(scene, camera)
+              requestAnimationFrame(animate)
+            }
+            animate()
+            
+            // element.style.transform =
+            //   "rotateZ(" + (e.alpha - 180) + "deg) " + 
+            //   "rotateX(" + e.beta + "deg) " +
+            //   "rotateY(" + -e.gamma + "deg)"
           })
         }
       })
